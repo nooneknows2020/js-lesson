75f8421b0971fa94a8e5107c214901bc2fea281a
nooneknows <e_dends@yahoo.co.j>
2023-08-25 16:03:59 +0900

    canvas要素をJSから生成し、表示するように変更

--- module/lesson9/main.js
+++ module/lesson9/main.js
@@ -1,63 +1,83 @@
 'use strict';
 
 // ボタンクリックでランダムな大きさの図形をブラウザに表示
+// ブラウザの初期表示は以下
+/*
+<div>
+<canvas id="canvas"></canvas>
+</div>
+<button id="btnSquare">Square</button>
+<button id="btnCircle">Circle</button>
+<button id="btnClear">CLEAR</button>
+<div>
+<h2>図形リスト</h2>
+<ul id="list"></ul>
+</div>
+*/
 import { Canvas } from './modules/canvas.js';
 import { Square } from './modules/square.js';
 import { Circle } from './modules/circle.js';
 
-// ブラウザ表示用
-const ulElem = document.getElementById('list');
-const btnSquare = document.getElementById('btnSquare');
-const btnCircle = document.getElementById('btnCircle');
 
 // キャンバスの生成
-const myCanvas = new Canvas(480, 320);
-const ctx = myCanvas.ctx;
-// console.log(ctx);
+const canvas = new Canvas(480, 320);
+// キャンバスの初期表示
+canvas.init();
 
 
-// 図形の情報をブラウザに表示
-function displayAreaPerimeter(shape){
-    const li = document.createElement('li');
-    let text;
-    if(shape.type == "SQUARE"){
-        text = `長方形:面積:${shape.getArea()}px, 外周:${shape.getPerimeter()}px`;       
-    }else{
-        text = `円:面積:${shape.getArea()}px, 円周:${shape.getPerimeter()}px`;    
-    }
+// // ブラウザ表示用
+// const ulElem = document.getElementById('list');
+// const btnSquare = document.getElementById('btnSquare');
+// const btnCircle = document.getElementById('btnCircle');
 
-    li.textContent = text;
-    ulElem.appendChild(li); 
-}
+// // キャンバスの生成
+// const myCanvas = new Canvas(480, 320);
+// const ctx = myCanvas.ctx;
+// // console.log(ctx);
 
-// ボタンをクリックしたときの処理
-btnSquare.addEventListener('click', function(){
-    // 図形の生成
-    // const square = new Square(10, 10, 50, 50, 'red');
-    // square.draw(ctx);
-    // ランダムな大きさの図形を生成
-    const square = new Square();
-    square.randomSquare(ctx);
-    // console.log(square);
 
-    displayAreaPerimeter(square);
-})
+// // 図形の情報をブラウザに表示
+// function displayAreaPerimeter(shape){
+//     const li = document.createElement('li');
+//     let text;
+//     if(shape.type == "SQUARE"){
+//         text = `長方形:面積:${shape.getArea()}px, 外周:${shape.getPerimeter()}px`;       
+//     }else{
+//         text = `円:面積:${shape.getArea()}px, 円周:${shape.getPerimeter()}px`;    
+//     }
 
-btnCircle.addEventListener('click', function(){
-    // const circle = new Circle(100, 100, 10, 'blue');
-    // circle.draw(ctx);
-    // ランダムな大きさの図形を生成
-    const circle = new Circle();
-    circle.randomCircle(ctx);
+//     li.textContent = text;
+//     ulElem.appendChild(li); 
+// }
 
-    displayAreaPerimeter(circle);
-})
+// // ボタンをクリックしたときの処理
+// btnSquare.addEventListener('click', function(){
+//     // 図形の生成
+//     // const square = new Square(10, 10, 50, 50, 'red');
+//     // square.draw(ctx);
+//     // ランダムな大きさの図形を生成
+//     const square = new Square();
+//     square.randomSquare(ctx);
+//     // console.log(square);
 
-btnClear.addEventListener('click', function(){
-    // キャンバスをリセット
-    ctx.reset();
-    // 図形リストを全削除
-    while(ulElem.firstChild){
-        ulElem.removeChild(ulElem.firstChild);
-    }
-})
+//     displayAreaPerimeter(square);
+// })
+
+// btnCircle.addEventListener('click', function(){
+//     // const circle = new Circle(100, 100, 10, 'blue');
+//     // circle.draw(ctx);
+//     // ランダムな大きさの図形を生成
+//     const circle = new Circle();
+//     circle.randomCircle(ctx);
+
+//     displayAreaPerimeter(circle);
+// })
+
+// btnClear.addEventListener('click', function(){
+//     // キャンバスをリセット
+//     ctx.reset();
+//     // 図形リストを全削除
+//     while(ulElem.firstChild){
+//         ulElem.removeChild(ulElem.firstChild);
+//     }
+// })
--- module/lesson9/modules/canvas.js
+++ module/lesson9/modules/canvas.js
@@ -1,11 +1,24 @@
 class Canvas{
     // コンストラクタ
     constructor(width, height){
-        this.canvasElem = document.getElementById('canvas');
-        this.canvasElem.width = width;
-        this.canvasElem.height = height;
-        
-        this.ctx = this.canvasElem.getContext("2d");
+        // canvaの生成
+        this.canvas = document.createElement("canvas");
+        document.body.appendChild(this.canvas);
+        // キャンバスのサイズが指定されていなければ、幅480、高さ480を初期値とする
+        this.canvas.width = width || 480;
+        this.canvas.height = height || 480;
+    }
+
+    init(){
+        const ctx = this.canvas.getContext("2d");
+        ctx.fillStyle = "#eeeeee";
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    draw(shape){
+        const ctx = this.canvas.getContext("2d");
+        console.log(shape);
+        shape.draw(this.canvas);
     }
 }
 
